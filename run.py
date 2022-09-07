2354a26f69f12bd1d1f8a277445f67e7acd1ef93
Sophia Wisdom <sophia.wisdom1999@gmail.com>
2022-09-07 06:44:20 +0000

    in progress changes

--- run.py
+++ run.py
@@ -6,10 +6,11 @@ import triton.language as tl
 true_states = []
 true_outputs = []
 
-STATE_SIZE = 64
+STATE_SIZE = 2048
 SEQUENCE_LENGTH = 32
-N_HEADS = 16
+N_HEADS = 1024
 
+'''
 state = torch.zeros((STATE_SIZE))
 A = torch.eye(STATE_SIZE, dtype=torch.float32, device="cuda")[None, :, :].repeat(N_HEADS, 1, 1)
 B = torch.randn((N_HEADS, STATE_SIZE, 1), dtype=torch.float32, device="cuda")
@@ -39,19 +40,24 @@ for i in range(N_HEADS):
         second_part = B[i].reshape(-1) * sequence[j]
         state = first_part + second_part
         torch_diag_outputs[(i, j)] = torch.sum(C[i] @ state, axis=0)
+'''
 
-def torch_no_diag(sequence, A, B, C, N_HEADS: int, STATE_SIZE: int):
-    torch_no_diag_outputs = torch.empty((N_HEADS, len(sequence)), dtype=torch.float32, device="cuda")
-    for i in range(N_HEADS):
-        state = torch.zeros((STATE_SIZE,), dtype=torch.float32, device="cuda")
-        for j in range(len(sequence)):
-            state = A[i] @ state + B[i] @ sequence[j]
-            torch_no_diag_outputs[(i, j)] = torch.sum(C[i] @ state)
+def torch_no_diag(sequence, A, B, C, N_HEADS: int, STATE_SIZE: int, SEQUENCE_LENGTH: int):
+    torch_no_diag_outputs = torch.empty((len(sequence), N_HEADS), dtype=torch.float32, device="cuda")
+    return torch_no_diag_outputs
+    state = torch.zeros((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
+    for j in range(SEQUENCE_LENGTH):
+        print(f"A.shape", A.shape, "state shape", state.shape, "b shape", B.shape, "sequence shape", sequence[j].shape)
+        state_move = A @ state
+        input_contribution = torch.einsum('hij,bj->bhi', B, sequence[j])
+        print("state_move shape", state_move.shape, "input_contribution shape", input_contribution.shape)
+        state = state_move + input_contribution
+        torch_no_diag_outputs[j] = C @ state
     return torch_no_diag_outputs
 
 torch_no_diag_script = torch.jit.script(torch_no_diag)
 
-def torch_diag(sequence, A_DIAG, B, C, N_HEADS: int, STATE_SIZE: int):
+def torch_diag(sequence, A_DIAG, B, C, N_HEADS: int, STATE_SIZE: int, SEQUENCE_LENGTH: int):
     torch_diag_outputs = torch.empty((N_HEADS, len(sequence)), dtype=torch.float32, device="cuda")
     for i in range(N_HEADS):
         state = torch.zeros((STATE_SIZE,), dtype=torch.float32, device="cuda")
@@ -62,7 +68,17 @@ def torch_diag(sequence, A_DIAG, B, C, N_HEADS: int, STATE_SIZE: int):
             torch_diag_outputs[(i, j)] = torch.sum(C[i] @ state)
     return torch_diag_outputs
 
-torch_diag_script = torch.jit.script(torch_diag)
+def torch_diag_batched(sequence, A_DIAG, B, C, N_HEADS: int, STATE_SIZE: int, SEQUENCE_LENGTH: int):
+    torch_diag_outputs = torch.empty((len(sequence), N_HEADS), dtype=torch.float32, device="cuda")
+    state = torch.zeros((N_HEADS, STATE_SIZE,), dtype=torch.float32, device="cuda")
+    for j in range(len(sequence)):
+        first_part = (A_DIAG * state)
+        second_part = B.reshape((B.shape[0], B.shape[1])) * sequence[j]
+        state = first_part + second_part
+        torch_diag_outputs[j] = torch.sum(C @ state)
+    return torch_diag_outputs
+
+torch_diag_script = torch.jit.script(torch_diag_batched)
 
 @triton.jit
 def ssm_kernel_perhead(u_ptr, a_ptr, b_ptr, c_ptr, output_ptr, U_LENGTH: tl.constexpr, N: tl.constexpr, N_HEADS: tl.constexpr):
@@ -79,11 +95,13 @@ def ssm_kernel_perhead(u_ptr, a_ptr, b_ptr, c_ptr, output_ptr, U_LENGTH: tl.cons
         value = tl.sum(X*C, axis=0)
         tl.store(output_ptr + (i * U_LENGTH + j), value)
 
+'''
 triton_outputs = torch.zeros((N_HEADS, SEQUENCE_LENGTH), device="cuda", dtype=torch.float32)
 assert sequence.is_cuda and A.is_cuda and B.is_cuda and C.is_cuda and triton_outputs.is_cuda
 ssm_kernel_perhead[(N_HEADS,)](sequence, A_DIAG, B, C, triton_outputs, SEQUENCE_LENGTH, STATE_SIZE, N_HEADS)
+'''
 
-def triton_ssm(sequence, A, B, C, N_HEADS, STATE_SIZE):
+def triton_ssm(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE, SEQUENCE_LENGTH: int):
     triton_outputs = torch.empty((N_HEADS, len(sequence)), device="cuda", dtype=torch.float32)
     ssm_kernel_perhead[(N_HEADS,)](sequence, A_DIAG, B, C, triton_outputs, len(sequence), STATE_SIZE, N_HEADS)
     return triton_outputs
@@ -93,15 +111,15 @@ def triton_ssm(sequence, A, B, C, N_HEADS, STATE_SIZE):
     triton.testing.Benchmark(
         x_names=['size'],  # argument names to use as an x-axis for the plot
         x_vals=[
-            2**i for i in range(12)
+            2**i for i in range(2, 14)
         ],  # different possible values for `x_name`
         x_log=True,  # x axis is logarithmic
         line_arg='provider',  # argument name whose value corresponds to a different line in the plot
-        line_vals=['triton', 'torch_script', 'no_diag_script'],  # possible values for `line_arg`
-        line_names=['Triton', 'TorchScriptdiag', 'TorchScript'],  # label name for the lines
-        styles=[('red', 'solid'), ("green", "-"), ("blue", "-")],  # line styles
+        line_vals=['triton', 'torch_script'],  # possible values for `line_arg`
+        line_names=['Triton', 'TorchScript'],  # label name for the lines
+        styles=[('red', 'solid'), ("blue", "solid"),],  # line styles
         ylabel='elem/s',  # label name for the y-axis
-        plot_name='ssm-performance',  # name for the plot. Used also as a file name for saving the plot.
+        plot_name=f'ssm-performance @ N={STATE_SIZE}, N_HEADS={N_HEADS}',  # name for the plot. Used also as a file name for saving the plot.
         args={},  # values for function arguments not in `x_names` and `y_name`
     )
 )
@@ -115,18 +133,14 @@ def benchmark(size, provider):
     outputs = torch.zeros((N_HEADS, SEQUENCE_LENGTH), dtype=torch.float32, device="cuda")
     sequence = torch.rand(size, dtype=torch.float32, device="cuda")[:, None]
 
-    print("called for size", size, "and provider", provider)
+    print("STATE_SIZE", STATE_SIZE, "called for length", size, "and provider", provider)
 
-    if provider == "no_diag":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_no_diag(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE))
-    elif provider == "no_diag_script":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_no_diag_script(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE))
-    elif provider == 'torch':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_diag(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE))
+    if provider == "no_diag_script":
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_no_diag_script(sequence, A, B, C, N_HEADS, STATE_SIZE, size))
     elif provider == "torch_script":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_diag_script(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE))
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_diag_script(sequence, A_DIAG, B, C.reshape((C.shape[0], C.shape[2])).T, N_HEADS, STATE_SIZE, size))
     elif provider == 'triton':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: triton_ssm(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE))
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: triton_ssm(sequence, A_DIAG, B, C, N_HEADS, STATE_SIZE, size))
     else:
         raise ValueError("got unknown provider", provider)
     elems = lambda ms: size/(ms/1000)
