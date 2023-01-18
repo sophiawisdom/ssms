3dc79cc9403144aeca42aef1ee47040e26e8dd3a
sophia wisdom <sophiawisdom1999@gmail.com>
2023-01-18 09:16:25 +0000

    correct implementation of triton

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from torch.utils.cpp_extension import load
 import triton
 import triton.language as tl
 
+'''
 print("compiling siso")
 result = subprocess.run(["ptxas", "--gpu-name=sm_80", "siso_forward.ptx", "siso_backward.ptx", "-o", "siso.o"])
 if result.returncode != 0:
@@ -18,6 +19,7 @@ print("completed compiling")
 print("about to compile")
 siso = load(name="siso", sources=["siso.cpp", "siso.cu"], extra_ldflags=["-lcuda"])
 print("compiled")
+'''
 
 @torch.jit.script
 def torch_diag(sequence, A_DIAG, B, C, N_HEADS: int, STATE_SIZE: int, SEQUENCE_LENGTH: int):
@@ -43,16 +45,16 @@ def ssm_kernel_perhead(u_ptr, a_ptr, b_ptr, c_ptr, output_ptr, SEQUENCE_LENGTH: 
     C = tl.load(c_ptr + i * N + tl.arange(0, N))
     X = tl.zeros((N,), dtype=tl.float32)
     for j in range(SEQUENCE_LENGTH):
-        u_k = tl.load(u_ptr + (j * N_HEADS))
+        idx = (i * SEQUENCE_LENGTH  + j)
+        u_k = tl.load(u_ptr + idx)
         X = X * A + B*u_k # X*A is N multiplies, B*u_k is N multiplies, adding is N adds
-        output_idx = ((j * N_HEADS) + i)
-        tl.store(output_ptr + output_idx, tl.sum(X*C, axis=0)) # X*C is N multiplies, summing is N adds
+        tl.store(output_ptr + idx, tl.sum(X*C, axis=0)) # X*C is N multiplies, summing is N adds
         # all told 2N FMAs and N multiplies
 
 runs = 0
 def triton_ssm(sequence, A, B, C, N_HEADS, STATE_SIZE, SEQUENCE_LENGTH):
     global runs
-    triton_outputs = torch.empty((SEQUENCE_LENGTH, N_HEADS), device=sequence.device, dtype=sequence.dtype)
+    triton_outputs = torch.empty((N_HEADS, SEQUENCE_LENGTH), device=sequence.device, dtype=sequence.dtype)
     asm = ssm_kernel_perhead[(N_HEADS,)](sequence, A, B, C, triton_outputs, SEQUENCE_LENGTH, STATE_SIZE, N_HEADS)
     if SEQUENCE_LENGTH == 8192:
         # runs += 1
@@ -60,7 +62,7 @@ def triton_ssm(sequence, A, B, C, N_HEADS, STATE_SIZE, SEQUENCE_LENGTH):
         if runs == 8:
             print(asm.asm["ptx"])
         # print(asm.asm["ptx"])
-    return triton_outputs, asm
+    return triton_outputs
 
 @triton.testing.perf_report(triton.testing.Benchmark(
         x_names=['SEQUENCE_LENGTH'],  # argument names to use as an x-axis for the plot
@@ -100,20 +102,20 @@ if len(sys.argv) > 1 and sys.argv[1] == "benchmark":
     sys.exit(0)
 
 print("Got to running first test")
-N_HEADS = 1
-STATE_SIZE = 32
-SEQUENCE_LENGTH = 32
-# A = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
-A = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
+N_HEADS = 64
+STATE_SIZE = 64
+SEQUENCE_LENGTH = 512
+A = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
+# A = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
 print("Created A")
-# B = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
-B = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
+B = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
+# B = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
 print("Created B")
-# C = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
-C = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
+C = torch.randn((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")/8
+# C = torch.empty((N_HEADS, STATE_SIZE), dtype=torch.float32, device="cuda")
 # print("Created A,B,C", A.abs().sum(), B.abs().sum(), C.abs().sum())
-# sequence = torch.randn((N_HEADS, SEQUENCE_LENGTH), dtype=torch.float32, device="cuda")/8
-sequence = torch.empty((N_HEADS, SEQUENCE_LENGTH), dtype=torch.float32, device="cuda")
+sequence = torch.randn((N_HEADS, SEQUENCE_LENGTH), dtype=torch.float32, device="cuda")/8
+# sequence = torch.empty((N_HEADS, SEQUENCE_LENGTH), dtype=torch.float32, device="cuda")
 
 if len(sys.argv) > 1 and sys.argv[1] == "torch":
     print("about to torch diag")
@@ -137,6 +139,8 @@ print("A,B,C", A.abs().sum(), B.abs().sum(), C.abs().sum())
 torch_output = torch_diag(sequence, A, B, C, N_HEADS, STATE_SIZE, SEQUENCE_LENGTH)
 print("torch sum output is", torch_output.to(dtype=torch.float64).abs().sum(), f"nan: {bool(torch_output.isnan().any())}")
 
-print(f"{torch.allclose(output, torch_output)=}")
+print(f"{torch.allclose(output, torch_output)=} {(output - torch_output).abs().sum()=}/{output.abs().sum()=}")
+print(output)
+print(torch_output)
 
 # breakpoint()
